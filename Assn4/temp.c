6b32862029749db49c603bd870dd1a1fd5c63f39
PranavNyati <pranavnyati26@gmail.com>
2023-02-11 10:26:51 +0530

    parse ip from url

---
+++ Assn4/temp.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <URL>\n", argv[0]);
+        return 1;
+    }
+
+    char *hostname = argv[1];
+    char *port = "80";
+    char *protocol = "http";
+    char *path = "/";
+
+    // Find hostname and path in the URL
+    char *p = strstr(hostname, "://");
+    if (p != NULL) {
+        protocol = strtok(hostname, "://");
+        hostname = p + 3;
+    }
+    p = strchr(hostname, '/');
+    if (p != NULL) {
+        path = p;
+        *p = '\0';
+    }
+    p = strchr(hostname, ':');
+    if (p != NULL) {
+        *p = '\0';
+        port = p + 1;
+    }
+
+    // Get the IP address of the host
+    struct addrinfo hints, *res;
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    int status = getaddrinfo(hostname, port, &hints, &res);
+    if (status != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+        return 2;
+    }
+
+    // Extract the IP address and port number
+    struct sockaddr_in *addr = (struct sockaddr_in *) res->ai_addr;
+    char ip_address[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &addr->sin_addr, ip_address, sizeof ip_address);
+    int port_number = ntohs(addr->sin_port);
+
+    // Print the results
+    printf("Protocol: %s\n", protocol);
+    printf("Hostname: %s\n", hostname);
+    printf("Path: %s\n", path);
+    printf("IP address: %s\n", ip_address);
+    printf("Port number: %d\n", port_number);
+
+    // Clean up
+    freeaddrinfo(res);
+    return 0;
+}
