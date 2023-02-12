3a2a6601f9b39e4d0528afee333ad7c9c1cca28b
jena-shreyas <jenashreyas@gmail.com>
2023-02-12 12:11:00 +0530

    GET request working with online HTTP site ;)

--- Assn4/MyBrowser.c
+++ Assn4/MyBrowser.c
@@ -135,11 +135,6 @@ int main(){
     const char *prompt = "MyBrowser> ";
     buf = (char *)malloc(BUF_SIZE * sizeof(char));
 
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
-        perror("Client socket could not be created!");
-        exit(0);
-    }
-
     while (1){
 
         printf("%s", prompt);
@@ -171,7 +166,7 @@ int main(){
         printf("Port : %d\n", req.port);
         printf("IP : %s\n", req.ip);
 
-        sprintf(request, "%s %s HTTP/1.1", req.cmd, req.url);
+        sprintf(request, "%s %s HTTP/1.1\r", req.cmd, req.url);
         strcat(request, "\nHost: ");
         strcat(request, req.host);
         strcat(request, "\nConnection: close");
@@ -209,8 +204,9 @@ int main(){
             strftime(buf, BUF_SIZE, "%a, %d %b %Y %H:%M:%S %Z", lt);
             strcat(request, "\nIf-Modified-Since: ");
             strcat(request, buf);
+            strcat(request, "\r\n\n");
 
-            printf("Request : \n\n%s\n", request);
+            printf("Request : \n\n%s", request);
         }
 
         else if (!strcmp(req.cmd, "PUT"))
@@ -229,7 +225,7 @@ int main(){
             strcat(request, "\nContent-Type: ");
             strcat(request, accept_type);
 
-            strcat(request, "\n\n");
+            strcat(request, "\r\n\n");
 
             char *file_content = (char *)malloc(size * sizeof(char));
             fread(file_content, sizeof(char), size, fp);
@@ -239,63 +235,67 @@ int main(){
             printf("Request : \n\n%s\n", request);
         }
 
-        // connect to server
-        // servaddr.sin_family = AF_INET;
-        // servaddr.sin_port = htons(req.port);
-        // inet_aton(req.ip, &servaddr.sin_addr);
+        if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+            perror("Client socket could not be created!");
+            exit(0);
+        }
 
-        // if ((connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr))) < 0)
-        // {
-        //     // printf("%d\n", errno);
-        //     perror("Client could not connect to server!");
-        //     exit(0);
-        // }
+        // connect to server
+        servaddr.sin_family = AF_INET;
+        servaddr.sin_port = htons(req.port);
+        inet_aton(req.ip, &servaddr.sin_addr);
+
+        if ((connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr))) < 0)
+        {
+            perror("Client could not connect to server!");
+            exit(0);
+        }
 
         // printf("Connected to server %s : %d\n", req.ip, req.port);
 
-        // char *request_ptr = request;
-        // //  send input in chunks
-        // while (1){
+        char *request_ptr = request;
+        //  send input in chunks
+        while (1){
 
-        //     memset(buf, '\0', BUF_SIZE);
-        //     strncpy(buf, request_ptr, BUF_SIZE);
-        //     request_ptr = request_ptr + BUF_SIZE;
+            memset(buf, '\0', BUF_SIZE);
+            strncpy(buf, request_ptr, BUF_SIZE);
+            request_ptr = request_ptr + BUF_SIZE;
 
-        //     send(sockfd, buf, BUF_SIZE, 0);
+            send(sockfd, buf, BUF_SIZE, 0);
 
-        //     if (strlen(buf) < BUF_SIZE)
-        //         break;
-        // }
+            if (strlen(buf) < BUF_SIZE)
+                break;
+        }
 
         memset(response, '\0', MAX_SIZE);
 
         // ***********SAMPLE TESTING OF PARSE_RESPONSE***********
-        strcpy(response, "HTTP/1.1 200 OK\n");
-        strcat(response, "Content-Type: text/html\n");
-        strcat(response, "Content-Length: 42\n");
-        strcat(response, "Connection: close\n");
-        strcat(response, "\n");
+        // strcpy(response, "HTTP/1.1 200 OK\n");
+        // strcat(response, "Content-Type: text/html\n");
+        // strcat(response, "Content-Length: 42\n");
+        // strcat(response, "Connection: close\n");
+        // strcat(response, "\n");
 
         // receive response from server
 
-        // printf("Response : \n\n");
-        // while (1){
+        printf("\nResponse : \n\n");
+        while (1){
 
-        //     memset(buf, '\0', BUF_SIZE);
-        //     recv(sockfd, buf, BUF_SIZE, 0);
-        //     printf("%s", buf);
-        //     strcat(response, buf);
+            memset(buf, '\0', BUF_SIZE);
+            recv(sockfd, buf, BUF_SIZE, 0);
+            printf("%s", buf);
+            // strcat(response, buf);
 
-        //     if (strlen(buf) < BUF_SIZE)
-        //         break;
-        // }
+            if (strlen(buf) < BUF_SIZE)
+                break;
+        }
 
         // // parse response
-        char *response_ptr = response;
+        // char *response_ptr = response;
         // memset(buf, '\0', BUF_SIZE);
 
-        parse_http_response(response_ptr);
-        // close(sockfd);
+        // parse_http_response(response_ptr);
+        close(sockfd);
     }
     return 0;
 }
