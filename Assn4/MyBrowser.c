71d6bd8e857cfbcff97629e580e551f069505253
jena-shreyas <jenashreyas@gmail.com>
2023-02-13 23:23:31 +0530

    Minor fixes in request, response display

--- Assn4/MyBrowser.c
+++ Assn4/MyBrowser.c
@@ -8,11 +8,13 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <poll.h>
 #include <time.h>
 #define CMD_SIZE 10
 #define BUF_SIZE 100
 #define URL_SIZE 300
 #define MAX_SIZE 2048
+#define TIMEOUT 3000
 
 typedef struct message_
 {
@@ -52,7 +54,7 @@ Message parse_request(char *input)
     Message req;
     char *token = strtok(input, " ");
     strcpy(req.cmd, token);
-    printf("cmd: %s\n", req.cmd);
+    // printf("cmd: %s\n", req.cmd);
 
     if (!strcmp(req.cmd, "GET"))
     {     
@@ -185,7 +187,6 @@ void parse_http_response(char *response)
 
 int main()
 {
-
     int sockfd;
     int i;
     struct sockaddr_in servaddr;
@@ -225,13 +226,13 @@ int main()
             break;
 
         Message req = parse_request(input);
-        printf("Command : %s\n", req.cmd);
-        printf("URL : %s\n", req.url);
-        printf("Host : %s\n", req.host);
-        printf("Port : %d\n", req.port);
-        printf("IP : %s\n", req.ip);
-        printf("File : %s\n", req.filename);
-        printf("Extension : %s\n", req.extension);
+        // printf("Command : %s\n", req.cmd);
+        // printf("URL : %s\n", req.url);
+        // printf("Host : %s\n", req.host);
+        // printf("Port : %d\n", req.port);
+        // printf("IP : %s\n", req.ip);
+        // printf("File : %s\n", req.filename);
+        // printf("Extension : %s\n", req.extension);
 
         char *request = (char *)malloc(MAX_SIZE * sizeof(char));
 
@@ -296,9 +297,8 @@ int main()
             strcat(request, accept_type);
             strcat(request, "\r\n\r\n");
 
-            printf("Beginning content writing \n\n\n");
+            // printf("Beginning content writing \n\n\n");
 
-            // ************************ CORRECT THIS PART, REMOVE strlen and use char-wise writing instead, also need to use realloc in loop ************************
             int offset = strlen(request);
             int request_size = MAX_SIZE;
             char *file_content = (char *)malloc(size * sizeof(char));
@@ -315,8 +315,9 @@ int main()
             }
             fclose(fp);
 
-            printf("File size : %d\n",size);
-            printf("Request : \n\n%s\n", request);
+            // printf("File size : %d\n",size);
+            printf("Request : \n\n");
+            fwrite(request, sizeof(char), offset, stdout);
         }
 
         if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
@@ -335,12 +336,11 @@ int main()
             perror("Client could not connect to server!");
             exit(0);
         }
-
         char *request_ptr = request;
+
         //  send input in chunks
         while (1)
         {
-
             memset(buf, '\0', BUF_SIZE);
             strncpy(buf, request_ptr, BUF_SIZE);
             request_ptr = request_ptr + BUF_SIZE;
@@ -357,57 +357,79 @@ int main()
         int bytes_recv = 0;
         int buf_recv;
 
-        while (1)
+        struct pollfd fdset[1];
+        fdset[0].fd = sockfd;
+        fdset[0].events = POLLIN;
+
+        int ret = poll(fdset, 1, TIMEOUT);
+
+        if (ret > 0)
         {
-            memset(buf, '\0', BUF_SIZE);
-            if ((buf_recv = recv(sockfd, buf, BUF_SIZE, 0)) == 0)
-                break;
-            while (response_size <= bytes_recv + buf_recv)
+            while (1)
             {
-                response_size *= 2;
-                response = (char *)realloc(response, response_size * sizeof(char));
+                memset(buf, '\0', BUF_SIZE);
+                if ((buf_recv = recv(sockfd, buf, BUF_SIZE, 0)) == 0)
+                    break;
+                while (response_size <= bytes_recv + buf_recv)
+                {
+                    response_size *= 2;
+                    response = (char *)realloc(response, response_size * sizeof(char));
+                }
+
+                // writing response from buffer
+                for (int i = 0; i < buf_recv; i++)
+                    response[bytes_recv + i] = buf[i];
+
+                bytes_recv += buf_recv;
             }
 
-            // writing response from buffer
-            for (int i = 0; i < buf_recv; i++)
-                response[bytes_recv + i] = buf[i];
-
-            bytes_recv += buf_recv;
-        }
-
-        if (!strcmp(req.cmd, "GET"))
-        {
-            FILE *fp = fopen(req.filename, "w");
-            char *body_beg_ptr = strstr(response, "\r\n\r\n");
-            size_t offset = (body_beg_ptr - response) / sizeof(char);
-
-            printf("Response : \n\n");
-            fwrite(response, sizeof(char), offset, stdout);
-            printf("\n\n");
-            fwrite(body_beg_ptr + 4, sizeof(char), bytes_recv - (offset + 4), fp);
-            fclose(fp);
-
-            if (fork() == 0)
+            if (!strcmp(req.cmd, "GET"))
             {
-                close(sockfd);
-                if (!strcmp(req.extension, ".html"))
-                    execlp("/usr/bin/firefox", "firefox", req.filename, NULL);
-                else if (!strcmp(req.extension, ".jpg"))
-                    execlp("/usr/bin/shotwell", "shotwell", req.filename, NULL);
-                else if (!strcmp(req.extension, ".pdf"))
-                    execlp("/usr/bin/xdg-open", "xdg-open", req.filename, NULL);
-                else
-                    execlp("/usr/bin/gedit", "gedit", req.filename, NULL);
-            }
-            wait(NULL); // wait for child process to finish
-        }
+                FILE *fp = fopen(req.filename, "w");
+                char *body_beg_ptr = strstr(response, "\r\n\r\n");
+                size_t offset = (body_beg_ptr - response) / sizeof(char);
 
-        else if (!strcmp(req.cmd, "PUT"))
-        {
-            printf("Response : \n\n");
-            fwrite(response, sizeof(char), bytes_recv, stdout);
-            printf("\n\n");
+                printf("Response : \n\n");
+                fwrite(response, sizeof(char), offset, stdout);
+                printf("\n\n");
+                fwrite(body_beg_ptr + 4, sizeof(char), bytes_recv - (offset + 4), fp);
+                fclose(fp);
+
+                if (fork() == 0)
+                {
+                    close(sockfd);
+                    if (!strcmp(req.extension, ".html"))
+                        execlp("/usr/bin/firefox", "firefox", req.filename, NULL);
+                    else if (!strcmp(req.extension, ".jpg"))
+                        execlp("/usr/bin/shotwell", "shotwell", req.filename, NULL);
+                    else if (!strcmp(req.extension, ".pdf"))
+                        execlp("/usr/bin/xdg-open", "xdg-open", req.filename, NULL);
+                    else
+                        execlp("/usr/bin/gedit", "gedit", req.filename, NULL);
+                }
+                wait(NULL); // wait for child process to finish
+            }
+
+            else if (!strcmp(req.cmd, "PUT"))
+            {
+                printf("Response : \n\n");
+                // printf("%s\n", response);
+                // fwrite(response, sizeof(char), bytes_recv, stdout);
+                // printf("\n\n");
+
+                char *body_beg_ptr = strstr(response, "\r\n\r\n");
+                size_t offset = (body_beg_ptr - response) / sizeof(char);
+
+                // printf("Response : \n\n");
+                fwrite(response, sizeof(char), offset, stdout);
+                printf("\n\n");
+
+            }
         }
+        else if (ret == 0)
+            printf("Timeout exceeded\n");
+        else
+            printf("Error in poll()\n");
 
         close(sockfd);
     }
