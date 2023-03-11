248ca9f762776774c52595ec551e92bbdb4317f6
PranavNyati <pranavnyati26@gmail.com>
2023-03-11 12:02:45 +0530

    started .h file

---
+++ Assn5/mysocket.h
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h> 
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <errno.h>
+#include <pthread.h>
+
+#define MAX_MESSAGE_SIZE 5000
+#define SOCK_MyTCP SOCK_STREAM
+
+int my_socket(int domain, int type, int protocol);
+int my_bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
+int my_listen(int sockfd, int backlog);
+int my_accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
+int my_connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
+int my_close(int sockfd);
+int my_send(int sockfd, const void *buf, size_t len, int flags);
+int my_recv(int sockfd, void *buf, size_t len, int flags);
+
+
+
