411f35aa992284a645bf9c4dda0a4c1d32cf44df
PranavNyati <pranavnyati26@gmail.com>
2023-03-11 12:16:24 +0530

    my_socket started and my_bind written

--- Assn5/mysocket.c
+++ Assn5/mysocket.c
@@ -1,2 +1,28 @@
 #include "mysocket.h"
 
+int my_socket(int domain, int type, int protocol)
+{
+    int sockfd;
+    if (type == SOCK_MyTCP){
+
+        if ((sockfd = socket(domain, SOCK_STREAM, protocol)) < 0) {
+            perror("Cannot create the socket!\n");
+        }   
+        return sockfd;
+    }
+
+    else {
+        perror("Invalid socket type!\n");
+        return -1;
+    }
+}
+
+int my_bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen)
+{
+    int bind_status;
+    if ((bind_status = bind(sockfd, addr, addrlen)) < 0) {
+        perror("Unable to bind local address!\n");
+    }
+    return bind_status;
+}
+
